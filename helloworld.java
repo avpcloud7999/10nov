d4e3e48b40f7319e21f8fbc55226bf3cef2eff73
Ashish <ashishpatil9770@gmail.com>
2025-11-10 17:08:51 +0530

    file edit

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Ashish,monu");
-=======
-        System.out.println("Ashish,Monu,Golu");
-        
->>>>>>> b1f09275a983c6b2b79b84d5c092523aa9cd03c7
+        System.out.println("Ashish Patil");
     }
 }
